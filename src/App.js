325215962970a5e628addf9e4e5a55f703aff5ce
Mark Daniel Delgado <mdelgado@pinterest.com>
2022-03-16 10:26:50 -0500

    first pull request

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Box, Button, Pulsar } from "gestalt";
+import "gestalt/dist/gestalt.css";
 
 function App() {
+  const [pulsarToggle, setPulsarToggle] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box margin={2}>
+      <Box>
+        {pulsarToggle && <Pulsar size={100} />}
+        <Button
+          fullWidth
+          color="red"
+          text="Test Button"
+          onClick={() => setPulsarToggle(!pulsarToggle)}
+        />
+      </Box>
+    </Box>
   );
 }
 
